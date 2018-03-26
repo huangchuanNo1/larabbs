d931a958f2651d85ad14bff0fe42726dd10bf273
Huang Chuan <845069743@qq.com>
2018-03-26 01:26:22 +0000

    add

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,5 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
-
+//一个话题一个分类
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+//一个话题一个作者
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
